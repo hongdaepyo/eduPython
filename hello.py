e769466660f74ed0193ebcf86c2185d086a92696
dphong <dphong@kaoni.com>
2019-05-30 11:06:12 +0900

    파이썬 클래스 공부

--- hello.py
+++ hello.py
@@ -1,11 +1,37 @@
-def add(a, b):
-    return a + b
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
 
-a = "python"
-b = " is fun"
+class MoreFourCal(FourCal):
+    def pow(self):
+        result = self.first ** self.second
+        return result
+    def div(self):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first / self.second
 
-print("=" * 50)
-print("multiple test")
-print("=" * 50)
+a = MoreFourCal(4, 0)
 
-print(a[3])
+print(a.add())
+print(a.mul())
+print(a.div())
+print(a.sub())
+print(a.pow())
